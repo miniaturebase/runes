a03a4b460d6114bf337bac0fffe542db93c1b3be
Jordan Brauer <jordan.brauer@pepper.com>
2019-11-15 15:26:01 -0600

    clean up helpers

--- src/helpers.php
+++ src/helpers.php
@@ -2,27 +2,38 @@
 
 declare(strict_types = 1);
 
-namespace UTFH8 {
-    if (!\function_exists('uniord')) {
-        function uniord(string $character)
-        {
-            $character = mb_convert_encoding($character, 'UCS-2LE', 'UTF-8');
+use UTFH8\Character;
+use UTFH8\Script;
 
-            return ord(substr($character, 1, 1)) * 256 + ord(substr($character, 0, 1));
-        }
+if (!\function_exists('uniord')) {
+    function uniord(string $character) {
+        $character = mb_convert_encoding($character, 'UCS-2LE', 'UTF-8');
+
+        return ord(substr($character, 1, 1)) * 256 + ord(substr($character, 0, 1));
     }
+}
 
-    if (!\function_exists('script')) {
-        function script(string $subject): Script
-        {
-            return new \UTFH8\Script($subject);
-        }
+if (!\function_exists('script')) {
+    /**
+     * Analyze and determine the scripts used in a given string.
+     *
+     * @param string $subject The string to analyze
+     * @return Script
+     */
+    function script(string $subject): Script {
+        return new Script($subject);
     }
+}
 
-    if (!\function_exists('char')) {
-        function char(string $character): Character
-        {
-            return new \UTFH8\Character($character);
-        }
+if (!\function_exists('char')) {
+    /**
+     * Analyze a character and call many helpful methods for interacting with
+     * the glyph.
+     *
+     * @param string $character A single string character to analyze
+     * @return Character
+     */
+    function char(string $character): Character {
+        return new Character($character);
     }
 }
